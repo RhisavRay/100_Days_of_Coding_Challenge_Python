33fa416dc477b8f07a1ef7c1ec71533b63a6988e
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-02-14 22:11:38 +0530

    Random number generator understood

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -1,7 +1,23 @@
 import random
-import my_module
+# import my_module
+# print(my_module.number)
 
-rand_int = random.randint(1, 10)
+# rand_int = random.randint(1, 10)
+# print(rand_int)
+# # Inclusive of both, upper and lower bound
 
-print(rand_int)
-print(my_module.number)
+# rand_fraction = random.random()
+# print(rand_fraction)
+# # Generates a random fraction/float num between 0 and 1, but ONLY inclusive of 0
+# # Can be extended for any range that looks like [0, x) by multiplying that value with x
+
+# rand_float = random.uniform(1, 10)
+# print(rand_float)
+# # Generates a random floating num. ONLY inclusive of lower bound
+
+rand_ht = random.randint(0, 1)
+
+if rand_ht == 0:
+    print("Heads")
+else:
+    print("Tails")
