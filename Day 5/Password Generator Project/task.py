5e7ada771650df8fab5b016fdef74909c3850917
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-02-14 23:38:03 +0530

    Correction made

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -44,7 +44,7 @@ random.shuffle(password_list)
 print(password_list)
 
 password = ""
-for char in password:
-    password += char\
+for char in password_list:
+    password += char
 
 print(f"Your password is: {password}")
