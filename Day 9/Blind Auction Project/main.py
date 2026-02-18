3de149c8cd826204bf0d49387bbd4b3e26d743bf
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-02-18 21:16:31 +0530

    Blind auction code done

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -2,5 +2,29 @@
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
+from platform import system
 
+import art
 
+print(art.logo)
+
+any_other = "yes"
+bids = {}
+
+while any_other == "yes":
+    name = input("What is your name?: ")
+    bid = input("What is your bid?: $")
+
+    bids[name] = int(bid)
+
+    any_other = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    print("\n" * 30)
+
+winner = ""
+winning_bid = 0
+for name in bids:
+    if bids[name] > winning_bid:
+        winner = name
+        winning_bid = bids[name]
+
+print(f"The winner is {winner} with a bid of ${winning_bid}")
