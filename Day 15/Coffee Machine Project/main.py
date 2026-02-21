eb06b3cf1a59b9377f762b7c2adc7991184920c8
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-02-22 00:07:03 +0530

    Coffee machine made

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -1,32 +1,27 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+import helper_data, helper_functions
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
 
+print("Welcome to the Coffee Machine App")
+
+repeat = True
+while repeat:
+    function = input("What would you like to do? Report or Order: ").lower()
+
+    if function == "report":
+        helper_functions.report()
+    elif function == "order":
+        order = helper_functions.take_order()
+        if order in helper_data.menu:
+            possible, ingredient = helper_functions.check_possibility(order)
+            if not possible:
+                print(f"Sorry. Can't make this order now. Not enough {ingredient} in the machine")
+            else:
+                helper_functions.place_order(order)
+        else:
+            print("That is not a valid option")
+    else:
+        print("Invalid entry")
+
+    repeat = True if input("Do you want to continue? Type 'yes' or 'no': ").lower() == "yes" else False
+
+print("Thank you for visiting the Coffee Machine App")
