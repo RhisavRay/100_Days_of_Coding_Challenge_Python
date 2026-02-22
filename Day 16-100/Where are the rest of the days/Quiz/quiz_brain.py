9084480e25e76825cb949e3d15f7ed1c5f4c890a
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-02-23 00:26:59 +0530

    Question brain created

---
+++ Day 16-100/Where are the rest of the days/Quiz/quiz_brain.py
@@ -0,0 +1,9 @@
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_no = 0
+        self.question_list = question_list
+
+    def next_question(self):
+        question = self.question_list[self.question_no]
+        answer = input(f"Q{self.question_no} {question.question}?(True/False): ").capitalize()
+        self.question_no += 1
