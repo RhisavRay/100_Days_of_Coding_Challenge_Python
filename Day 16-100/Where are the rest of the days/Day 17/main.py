64fbd99d41629a21399d0650671a16315e212f4b
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-02-22 23:38:14 +0530

    Methods of a class

--- Day 16-100/Where are the rest of the days/Day 17/main.py
+++ Day 16-100/Where are the rest of the days/Day 17/main.py
@@ -3,9 +3,20 @@ class User:
         self.user_id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
 
-user1 = User(1, "user1")
+    def follow(self, user):
+        self.following += 1
+        user.followers += 1
 
-print(user1.user_id)
-print(user1.username)
-print(user1.followers)
+user1 = User(1, "Patrik")
+user2 = User(2, "Spongebob")
+
+print(f"User: {user1.username}\nFollowers: {user1.followers}\nFollowing: {user1.following}\n")
+print(f"User: {user2.username}\nFollowers: {user2.followers}\nFollowing: {user2.following}")
+
+user1.follow(user2)
+print(f"\n\n{user1.username} followed {user2.username}\n\n")
+
+print(f"User: {user1.username}\nFollowers: {user1.followers}\nFollowing: {user1.following}\n")
+print(f"User: {user2.username}\nFollowers: {user2.followers}\nFollowing: {user2.following}")
