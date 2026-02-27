670bde7ca81be1da54c721a36aaeed5af29945eb
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-02-27 21:16:32 +0530

    Exercise 4 on turtles done

--- Day 16-100/Where are the rest of the days/Day 18/main.py
+++ Day 16-100/Where are the rest of the days/Day 18/main.py
@@ -8,13 +8,18 @@ timmy.shape("turtle")
 # Change the color of the turtle
 timmy.color("red")
 
+colors = ["red", "yellow", "green", "blue", "purple", "cornflower blue", "orange", "pink", "green yellow", "dark orange"]
+
 # Move the tutle 100 units to the front
 # the basic forward, backward, left and right commands have shorthand versions as well. They work exactly the same way.
 # Just they are smaller commands to write
 # timmy.forward(100)
 # Turn Timmy to the right. This function takes degrees as the argument
 # timmy.left(90)
+
+
 # Make Timmy draw a square
+
 # timmy.forward(100)
 # timmy.left(90)
 # timmy.forward(100)
@@ -28,6 +33,7 @@ timmy.color("red")
 
 
 # Timmy draws a dashed line
+
 # timmy.up()
 # timmy.backward(500)
 # timmy.down()
@@ -39,6 +45,7 @@ timmy.color("red")
 
 
 # Timmy draws triangle, sqaure, pentagon, hexagon, and so on till 10 sides
+
 # timmy.up()
 # timmy.right(90)
 # timmy.backward(200)
@@ -50,6 +57,19 @@ timmy.color("red")
 #     for _ in range(i):
 #         timmy.forward(100)
 #         timmy.rt(360/i)
+
+
+# Timmy draws a random walk
+
+timmy.width(10)
+timmy.speed(100)
+
+angles = [0, 90, 180, 270]
+
+for _ in range(100):
+    timmy.color(random.choice(colors))
+    timmy.left(random.choice(angles))
+    timmy.forward(30)
 
 
 screen = Screen()
