0a206710a7a1e9c14fba00adac267a30b352eabc
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-02 10:52:46 +0530

    Prerequisites of the game done

---
+++ Day 16-100/Where are the rest of the days/Day 20-21/Snake game.py
@@ -0,0 +1,40 @@
+from turtle import Turtle, Screen
+import time
+
+screen = Screen()
+# Make screen background black
+screen.bgcolor("black")
+
+# TODO 1: Create the snake
+
+
+
+# TODO 2: Make the snake move forward till GAME OVER (This logic to be added later)
+
+
+
+# TODO 3: Use arrow keys to change the heading of the snake
+
+
+
+# TODO 4: Make food dots in random location on the screen
+
+
+
+# TODO 5: Upon eating the food, another food dot appears at random on the screen
+
+
+
+# TODO 6: Make the snake grow upon eating the food
+
+
+
+# TODO 7: Add score feature
+
+
+
+# TODO 8: Add the GAME OVER logic (Hitting wall or biting own tail)
+
+
+
+screen.exitonclick()
