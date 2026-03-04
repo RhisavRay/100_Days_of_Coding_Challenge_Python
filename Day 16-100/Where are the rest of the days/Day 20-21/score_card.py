82c82462b6c1e3be4711be7b7f9b6c4b57e6c19c
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-05 02:34:48 +0530

    Initial score style setup logic

--- Day 16-100/Where are the rest of the days/Day 20-21/score_card.py
+++ Day 16-100/Where are the rest of the days/Day 20-21/score_card.py
@@ -2,11 +2,16 @@ from turtle import Turtle
 
 class ScoreCard:
     def __init__(self):
-        self.score = 0
         self.score = Turtle()
         self.score_setup()
         self.score_value = 0
         self.show_score()
+
+    def score_setup(self):
+        self.score.hideturtle()
+        self.score.up()
+        self.score.goto(0, 310)
+        self.score.color("white")
 
 
     def gain_score(self):
