5471c2ba88e20167688a50da833327d021e7dab4
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-05 02:32:45 +0530

    Added Wall collision logic

--- Day 16-100/Where are the rest of the days/Day 20-21/snake.py
+++ Day 16-100/Where are the rest of the days/Day 20-21/snake.py
@@ -21,23 +21,42 @@ class Snake:
             turtle.goto(INIT_POS[i], 0)
             self.snake.append(turtle)
 
-    def turn_right(self):
-        if self.head.heading() != LEFT:
-            self.head.setheading(RIGHT)
-    
-    def turn_left(self):
-        if self.head.heading() != RIGHT:
-            self.head.setheading(LEFT)
-    
-    def turn_up(self):
-        if self.head.heading() != DOWN:
-            self.head.setheading(UP)
-    
-    def turn_down(self):
-        if self.head.heading() != UP:
-            self.head.setheading(DOWN)
-    
     def move(self):
         for i in range(len(self.snake) - 1, 0, -1):
             self.snake[i].goto(self.snake[i - 1].pos())
         self.head.fd(20)
+
+    def turn_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def turn_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def turn_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def turn_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    @staticmethod
+    def create_segment(position):
+        new_segment = Turtle()
+        new_segment.shape("square")
+        new_segment.color("white")
+        new_segment.up()
+        new_segment.goto(position)
+        return new_segment
+
+    def grow_snake(self):
+        new_position = self.snake[-1].pos()
+        new_segment = self.create_segment(new_position)
+        self.snake.append(new_segment)
+
+    def wall_collision(self):
+        if abs(self.head.xcor()) > 300 or abs(self.head.ycor()) > 300:
+            return True
+        return False
