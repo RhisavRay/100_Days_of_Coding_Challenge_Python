82803fe94a661543ed4f1dd901dab9ddad85e518
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-05 02:33:03 +0530

    Added biting logic

--- Day 16-100/Where are the rest of the days/Day 20-21/snake_game.py
+++ Day 16-100/Where are the rest of the days/Day 20-21/snake_game.py
@@ -1,10 +1,10 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 from snake import Snake
 from food import Food
 from score_card import ScoreCard
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=680, height=680)
 screen.bgcolor("black")
 screen.tracer(0)
 
@@ -20,15 +20,29 @@ food = Food()
 
 score_card = ScoreCard()
 
+border = Turtle()
+border.penup()
+border.goto(-310, -310)
+border.pendown()
+border.pencolor("white")
+for _ in range(4):
+    border.fd(620)
+    border.left(90)
+border.hideturtle()
+
 game_is_on = True
 
 def game_loop():
     global game_is_on
     if game_is_on:
         snake.move()
+        # print(snake.head.pos())
         if food.collision_detection(snake.head):
             score_card.gain_score()
-            print(score_card.score)
+            snake.grow_snake()
+        if snake.wall_collision() or snake.bite_check():
+            game_is_on = False
+            score_card.game_over()
         screen.update()
         screen.ontimer(game_loop, 100)
 
