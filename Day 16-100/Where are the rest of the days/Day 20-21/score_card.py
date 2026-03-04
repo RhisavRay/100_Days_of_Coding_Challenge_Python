46dffcbfae0c3837877cb912c81ff445e34b67e3
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-05 02:34:15 +0530

    Improved score card logic

--- Day 16-100/Where are the rest of the days/Day 20-21/score_card.py
+++ Day 16-100/Where are the rest of the days/Day 20-21/score_card.py
@@ -1,6 +1,13 @@
+from turtle import Turtle
+
 class ScoreCard:
     def __init__(self):
         self.score = 0
+        self.score = Turtle()
+        self.score_setup()
+        self.score_value = 0
+        self.show_score()
+
 
     def gain_score(self):
         self.score += 1
