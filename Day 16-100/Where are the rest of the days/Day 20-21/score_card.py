f146f171a0022e9831591022ed48054395100c62
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-07 02:39:46 +0530

    Removed the setup for the score card. Was redundent

--- Day 16-100/Where are the rest of the days/Day 20-21/score_card.py
+++ Day 16-100/Where are the rest of the days/Day 20-21/score_card.py
@@ -3,15 +3,12 @@ from turtle import Turtle
 class ScoreCard:
     def __init__(self):
         self.score = Turtle()
-        self.score_setup()
-        self.score_value = 0
-        self.show_score()
-
-    def score_setup(self):
         self.score.hideturtle()
         self.score.up()
         self.score.goto(0, 310)
         self.score.color("white")
+        self.score_value = 0
+        self.show_score()
 
     def show_score(self):
         self.score.clear()
