07cea90088707fdbd5268a806993452c3ecfb5b2
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-07 20:56:19 +0530

    Given directional control

--- Day 16-100/Where are the rest of the days/Day 23/turtle_crossing.py
+++ Day 16-100/Where are the rest of the days/Day 23/turtle_crossing.py
@@ -13,6 +13,20 @@ screen.listen()
 
 player = Player()
 
+directions = {"Up": False, "Down": False}
+
+def key_press(key):
+    directions[key] = True
+
+def key_release(key):
+    directions[key] = False
+
+screen.onkeypress(lambda: key_press("Up"), "Up")
+screen.onkeyrelease(lambda: key_release("Up"), "Up")
+screen.onkeypress(lambda: key_press("Down"), "Down")
+screen.onkeyrelease(lambda: key_release("Down"), "Down")
+
+
 game_is_on = True
 
 def game_loop():
