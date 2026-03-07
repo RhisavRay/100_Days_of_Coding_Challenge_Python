e00fee3afc86d2ffeb426d16ec57bb11f9f63039
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-07 21:01:38 +0530

    Car setup

--- Day 16-100/Where are the rest of the days/Day 23/lane.py
+++ Day 16-100/Where are the rest of the days/Day 23/lane.py
@@ -12,3 +12,13 @@ class Lane(Turtle):
         self.side = side
         self.y = y
         self.car_setup()
+
+    def car_setup(self):
+        self.car.color_random()
+        self.car.dist = random.randint(5, 20)
+        if self.side == "L":
+            self.car.goto(-320, self.y)
+            self.car.setheading(0)
+        elif self.side == "R":
+            self.car.goto(320, self.y)
+            self.car.setheading(180)
