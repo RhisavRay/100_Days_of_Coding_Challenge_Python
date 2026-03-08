85970cf8a14cfba5d62560229fc16db8a10c6981
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-08 22:42:50 +0530

    Updates in score card to read the file and maeke changes if needed

--- Day 16-100/Where are the rest of the days/Day 20-21/score_card.py
+++ Day 16-100/Where are the rest of the days/Day 20-21/score_card.py
@@ -8,16 +8,26 @@ class ScoreCard:
         self.score.goto(0, 310)
         self.score.color("white")
         self.score_value = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.show_score()
 
     def show_score(self):
         self.score.clear()
-        self.score.write(f"Score: {self.score_value}", align="center", font=("Arial", 16, "normal"))
+        self.score.write(f"Score: {self.score_value}  High Score: {self.high_score}", align="center", font=("Arial", 16, "normal"))
 
     def gain_score(self):
         self.score_value += 1
         self.show_score()
 
+    def update_highscore(self):
+        if self.score_value > self.high_score:
+            self.high_score = self.score_value
+        with open("data.txt", "w") as data:
+            data.write(str(self.high_score))
+        self.score_value = 0
+        self.show_score()
+
     def game_over(self):
         self.score.clear()
-        self.score.write(f"GAME OVER  ---  Final Score: {self.score_value}", align="center", font=("Arial", 16, "normal"))
+        self.score.write(f"GAME OVER  ---  Your Score: {self.score_value}  Highscore: {self.high_score}", align="center", font=("Arial", 16, "normal"))
