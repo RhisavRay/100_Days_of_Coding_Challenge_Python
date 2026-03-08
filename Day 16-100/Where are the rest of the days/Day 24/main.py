34bc603da27499b1aadf5e26fcbd5cb495f87c8d
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-08 22:31:00 +0530

    File handling with Python understood

---
+++ Day 16-100/Where are the rest of the days/Day 24/main.py
@@ -0,0 +1,22 @@
+"""
+open is a default keyword which allows opening of files from the disc
+But always remember to close the file after opening
+"""
+
+# file = open("My_text.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# with open("My_text.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# with open("My_text.txt", "w") as file:
+#     my_file.write("New text!!!")
+
+# with open("My_text.txt", "a") as file:
+#     file.write("\nNew text!!!")
+
+# with open("new_file.txt", "w") as new_file:
+#     new_file.write("New text in new file")
